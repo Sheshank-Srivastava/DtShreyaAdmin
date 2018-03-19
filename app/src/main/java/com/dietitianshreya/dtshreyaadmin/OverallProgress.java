375d01e5fb0a8bb4e1b225d5cea7cc1ab7f2e38f
Manya Madan <developers.manyaparas@gmail.com>
2018-03-19 19:09:21 +0530

    progress

---
+++ app/src/main/java/com/dietitianshreya/dtshreyaadmin/OverallProgress.java
@@ -0,0 +1,13 @@
+package com.dietitianshreya.dtshreyaadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverallProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overall_progress);
+    }
+}
